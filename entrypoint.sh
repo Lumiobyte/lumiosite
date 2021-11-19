4b7fe6e6ae5a93c5131b04a827afe42602506204
Lumiobyte <evanpartridge0@gmail.com>
2021-11-19 12:33:26 +1100

    Add chown to entrypoint change emal

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'goodpassword')" | python3 manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'evanpartridge0@gmail.com', 'goodpassword')" | python3 manage.py shell
+
+chown www-data db.sqlite3
 
 gunicorn lumiobytenet.wsgi:application --bind 0.0.0.0:8000
