fdb72d58c746fe5cf0bc913b86c89c8fde494d78
Lumiobyte <evanpartridge0@gmail.com>
2021-11-19 12:24:47 +1100

    Add superuser creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'goodpassword')" | python3 manage.py shell
 
 gunicorn lumiobytenet.wsgi:application --bind 0.0.0.0:8000
